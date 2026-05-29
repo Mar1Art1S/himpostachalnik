076ac8bf070bdbdc774a4a6c1711d5ea1b6cbc4c
Mar1Art1S <mar1art1s@example.com>
2026-05-29 12:22:09 +0300

    feat: pidklucheno Google Analytics (G-5LWQN85SZJ)

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -110,18 +110,16 @@
             max-width: 100vw;
         }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5LWQN85SZJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-5LWQN85SZJ');
+    </script>
 </head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7WSPMVQ2DB"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'G-7WSPMVQ2DB');
-</script>
 
 <body>
     <!-- Google Tag Manager (noscript) -->
