6f7b8acb8bdba7c6a32e7ab22ac3fc98e1809363
Mar1Art1S <mar1art1s@example.com>
2026-05-29 12:12:08 +0300

    feat: pidklucheno Google Tag Manager (GTM-PWKM8WML)

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,30 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PWKM8WML');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="google-site-verification" content="YI1qHQLqb3Zb5gUi9zIJLM3tP2jW81Vu7pd_0GzUbOs" />
-
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NW4654RH');
-    </script>
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW4654RH" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-
 
     {!! seo($article ?? null) !!}
 
@@ -137,6 +124,10 @@
 </script>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWKM8WML"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="loader">
         <div class="loader-spinner">
             <x-lucide-settings class="loader-icon size-10" />
